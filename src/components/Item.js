5ae2fd765ce0d05f6d496f71150fa6fb8ac9f725
Harley Turan <harley@hturan.com>
2016-03-05 16:22:03 +0000

    Updated list styles

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,8 @@ export default class Item extends React.Component {
   render() {
     return (
       <section className="item">
-        <a className="item-link" href={this.props.url} target="_blank">{this.props.title}</a> — {this.props.currency}{this.props.amount}
+        <span className="item-price">{this.props.currency}{ this.props.amount.toString().indexOf('.') > -1 ? this.props.amount : `${this.props.amount}.00` }</span>
+        <span className="item-title"><a className="item-link" href={this.props.url} target="_blank">{this.props.title}</a></span>
         <a className="delete-link" onClick={this.props.handleItemDelete}>✖</a>
       </section>
     );
